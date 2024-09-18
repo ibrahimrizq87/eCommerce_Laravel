351dae509852ce4fba621c3642f65467f01e662b
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-09-18 16:36:54 +0300

    registration, contact and login forms created

--- angular-front/src/app/components/register/register.component.ts
+++ angular-front/src/app/components/register/register.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CityListComponent } from './city-list/city-list.component';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CountryListComponent,
+    CityListComponent,
+    CommonModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
 
+export class RegisterComponent  {
+
+  
 }
+
