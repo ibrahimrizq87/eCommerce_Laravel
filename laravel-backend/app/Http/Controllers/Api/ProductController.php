7433702018ddc38219d22f7f2727e8a2484da222
HadeerAli123 <hadeerabdelaziz2024@gmail.com>
2024-09-22 23:06:11 +0300

    store and update products

--- laravel-backend/app/Http/Controllers/Api/ProductController.php
+++ laravel-backend/app/Http/Controllers/Api/ProductController.php
@@ -21,77 +21,116 @@ class ProductController extends Controller
     }
     
     
-    
     public function store(Request $request)
-{
-        $validator = Validator::make($request->all(), [
-            'product_name' => 'required|string|max:255',
-            'price' => 'required|numeric',
-            'description' => 'nullable|string',
-            'images' => 'nullable|array',
-            'images.*' => 'file|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'videos' => 'nullable|array',
-            'videos.*' => 'file|mimes:mp4,mkv,avi|max:102400',
-            'stock' => 'required|integer',
-            'category_id' => 'required|integer|exists:categories,id',
-            'user_id' => 'required|integer|exists:users,id',
-        ], [
-            'product_name.required' => 'Product name is required.',
-            'product_name.string' => 'Product name must be a string.',
-            'product_name.max' => 'Product name may not be greater than 255 characters.',
-            'price.required' => 'Price is required.',
-            'price.numeric' => 'Price must be a number.',
-            'description.string' => 'Description must be a string.',
-            'images.file' => 'Image must be a file.',
-            'images.image' => 'The file must be an image.',
-            'images.mimes' => 'Image must be of type: jpeg, png, jpg, or gif.',
-            'images.max' => 'Image may not be greater than 2 MB.',
-            'videos.file' => 'Video must be a file.',
-            'videos.mimes' => 'Video must be of type: mp4, mkv, or avi.',
-            'videos.max' => 'Video may not be greater than 40 MB.',
-            'stock.required' => 'Stock is required.',
-            'stock.integer' => 'Stock must be an integer.',
-            'category_id.required' => 'Category ID is required.',
-            'category_id.integer' => 'Category ID must be an integer.',
-            'category_id.exists' => 'Category ID does not exist.',
-        ]);
+    {
+        // dd($request->headers->all());
+        // return response()->json(['data' => $request->all() ,'header' => $request->headers->all()],200);
+        // return response()->json(['data' => $request->all() ,'header' => $request->headers->all()],200);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
+        try {
+            $validator = Validator::make($request->all(), [
+                'product_name' => 'required|string|max:255',
+                'size' => 'required|string|max:255',
+                'material' => 'required|string|max:255',
+                'cover_image' => 'required|file|image|mimes:jpeg,png,jpg,gif|max:2048',
 
-        $data = $validator->validated();
-        $category = Category::find($data['category_id']);
-        if (!$category) {
-            return response()->json(['error' => 'Invalid category ID'], 400);
-        }
-
-        $imagePaths = [];
-        $videoPaths = [];
-
-        if ($request->hasFile('images')) {
-            Storage::disk('public')->makeDirectory('products');
-            foreach ($data['images'] as $image) {
-                $path = $image->store('products', 'public');
-                $imagePaths[] = $path;
+                'price' => 'required|numeric',
+                'description' => 'nullable|string',
+                'images' => 'nullable|array',
+                'images.*' => 'nullable|file|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'video' => 'nullable|file|mimes:mp4,mkv,avi,webm|max:102400',
+                'stock' => 'required|integer',
+                'category_id' => 'required|integer|exists:categories,id',
+            ], [
+                'product_name.required' => 'Product name is required.',
+                'product_name.string' => 'Product name must be a string.',
+                'product_name.max' => 'Product name may not be greater than 255 characters.',
+                'price.required' => 'Price is required.',
+                'price.numeric' => 'Price must be a number.',
+                'description.string' => 'Description must be a string.',
+                'images.file' => 'Image must be a file.',
+                'images.image' => 'The file must be an image.',
+                'images.mimes' => 'Image must be of type: jpeg, png, jpg, or gif.',
+                'images.max' => 'Image may not be greater than 2 MB.',
+                'video.file' => 'Video must be a file.',
+                'video.mimes' => 'Video must be of type: mp4, mkv, or avi.',
+                'video.max' => 'Video may not be greater than 40 MB.',
+                'stock.required' => 'Stock is required.',
+                'stock.integer' => 'Stock must be an integer.',
+                'category_id.required' => 'Category ID is required.',
+                'category_id.integer' => 'Category ID must be an integer.',
+                'category_id.exists' => 'Category ID does not exist.',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()],400);
             }
-            $data['images'] = implode(',', $imagePaths);
-        }
+    
+            $data = $validator->validated();
 
-        if ($request->hasFile('videos')) {
-            Storage::disk('public')->makeDirectory('videos');
-            foreach ($data['videos'] as $video) {
-                $path = $video->store('videos', 'public');
-                $videoPaths[] = $path;
+            $category = Category::find($data['category_id']);
+
+            if (!$category) {
+                return response()->json(['error' => 'Invalid category ID'], 400);
             }
-            $data['videos'] = implode(',', $videoPaths);
+    
+            $video_path ='';
+            if ($request->hasFile('video')) {
+
+                
+                    $path = $data['video'] ->store('videos', 'products');
+                    $path= asset('uploads/products/' . $path); 
+                    $video_path = $path;
+               
+            }
+            $image_path ='';
+            if ($request->hasFile('cover_image')) {
+
+                
+                    $path = $data['cover_image'] ->store('cover_images', 'products');
+                    $path= asset('uploads/products/' . $path); 
+                    $image_path = $path;
+               
+            }
+
+            $product = new Product();
+            $product->product_name = $data['product_name'];
+            $product->price = $data['price'];
+            $product->description = $data['description'];
+            $product->stock = $data['stock'];
+            $product->video = $video_path;
+            $product->cover_image = $image_path;
+
+            $product->user_id = Auth::id();
+            $product->category_id = $data['category_id']; 
+            $product->size = $data['size']; 
+            $product->material = $data['material']; 
+            $product->save();
+
+            
+
+            // return response()->json(['data' => $request->all() ,'header' => $request->headers->all()],200);
+
+
+            if ($request->hasFile('images')) {
+                foreach ($data['images'] as $image) {
+                    $path = $image->store('images', 'products');
+                    $path= asset('uploads/products/' . $path); 
+                    $productImage = new ProductImage();
+                    $productImage->product_id =  $product->id;
+                    $productImage->image = $path;
+                    $productImage->save();
+                }
+            }
+         
+
+
+    
+            return response()->json(['message' => 'Product created successfully', 'product' => $product , 'product' => $video_path], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-
-        $product = Product::create($data);
-
-        return response()->json(['message' => 'Product created successfully', 'product' => $product], 201);
-    }
-
+        
+        }
 
     public function update(Request $request, Product $product)
     {
