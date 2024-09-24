81c31e69f6293b5bec4933724303ef538dd0190a
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-09-24 22:03:18 +0300

    wishlist page is updated

--- angular-front/src/app/components/wishlist/wishlist.component.ts
+++ angular-front/src/app/components/wishlist/wishlist.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CustomerHeaderComponent } from "../customer-header/customer-header.component";
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [CustomerHeaderComponent],
+  imports: [CustomerHeaderComponent, RouterModule],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css'
 })
