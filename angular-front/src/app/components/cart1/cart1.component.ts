ae5634305e9754339dc3ec206e4fba7151b1cb02
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-09-26 02:21:34 +0300

    checkout page created

--- angular-front/src/app/components/cart1/cart1.component.ts
+++ angular-front/src/app/components/cart1/cart1.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomerHeaderComponent } from "../customer-header/customer-header.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart1',
   standalone: true,
-  imports: [CustomerHeaderComponent],
+  imports: [CustomerHeaderComponent, RouterModule],
   templateUrl: './cart1.component.html',
   styleUrl: './cart1.component.css'
 })
