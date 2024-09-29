f0e9cbcdd5ef2e6d9ed7db70e987558ab740c420
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-09-29 20:17:24 +0300

    admin view product is created

---
+++ angular-front/src/app/components/admin-page/admin-view-product/admin-view-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-view-product.component.html',
+  styleUrl: './admin-view-product.component.css'
+})
+export class AdminViewProductComponent {
+
+}
