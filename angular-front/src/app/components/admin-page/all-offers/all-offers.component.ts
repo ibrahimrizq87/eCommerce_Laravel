c7649296fa32454ec5636b8e0bfb2765dbef70b1
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-10-03 21:28:29 +0300

    adding pagination and search bar into admin dashboard

--- angular-front/src/app/components/admin-page/all-offers/all-offers.component.ts
+++ angular-front/src/app/components/admin-page/all-offers/all-offers.component.ts
@@ -3,17 +3,20 @@ import { RouterModule } from '@angular/router';
 import { UpdateProductComponent } from '../../seller-page/update-product/update-product.component';
 import { CommonModule } from '@angular/common';
 import { OfferService } from '../../../services/offer.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 
 @Component({
   selector: 'app-all-offers',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './all-offers.component.html',
   styleUrl: './all-offers.component.css'
 })
 export class AllOffersComponent {
   offers: any; 
-
+  page: number = 1;              
+  itemsPerPage: number = 10; 
   constructor(private offerService: OfferService) { }
 
 
